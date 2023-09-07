104cd482bffb3626c1a0b10ee69f6a65b08f238c
Kyle Cathers <kyle_c@live.ca>
2023-09-06 21:03:35 -0700

    refactor menu page

--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,46 @@ import chickenBurgerPic from './images/chickenBurger.jpg';
 import salmonBurgerPic from './images/salmonBurger.jpg';
 
 function menu() {
+    const menuList = [
+        {
+            name: "Breakfast", // ❖
+            price: 13,
+            ingredients: ["Fried Egg", "White Cheddar Cheese", "Lettuce", "Bacon",
+                            "Tomato", "Caramelized Onion", "Toasted Brioche Bun", "Bacon Aoili"],
+            picture: breakfastBurgerPic
+        },
+
+        {
+            name: "Beef", // ❖
+            price: 18,
+            ingredients: ["Double Beef Patty", "White Cheddar Cheese", "Lettuce", "Red Onion",
+                            "Toasted Sesame Seed Bun", "Pickles", "Tomato", "Garlic Aoili"],
+            picture: beefBurgerPic
+        },
+
+        {
+            name: "Bison", // ❖
+            price: 17,
+            ingredients: ["Grass Fed Bison Patty", "Swiss Cheese", "Lettuce", "Tomato", "Caramelized Onions",
+                            "Toasted Brioche Bun", "Jalepeno Aoili"],
+            picture: bisonBurgerPic
+        },
+
+        {
+            name: "Chicken", // ❖
+            price: 15,
+            ingredients: ["Double Fried Chicken", "Cheddar Cheese", "Lettuce", "Toasted Brioche Bun", "Sriracha Aoili"],
+            picture: chickenBurgerPic
+        },
+
+        {
+            name: "Salmon", // ❖
+            price: 16,
+            ingredients: ["Wild Salmon Patty", "Red Onion", "Lettuce", "Tomato", "Toasted Brioche Bun", "Remoulade Sauce"],
+            picture: salmonBurgerPic
+        },
+    ];
+
     const menuContent = document.createElement('div');
     menuContent.setAttribute('id', 'menu-content');
 
@@ -27,156 +67,9 @@ function menu() {
     const menuBox = document.createElement('div');
     menuBox.setAttribute('id', 'menu-box');
 
-    // breakfast burger
-
-    const breakfastBurger = document.createElement('div');
-    breakfastBurger.setAttribute('id', 'breakfast-burger');
-    breakfastBurger.setAttribute('class', 'burger');
-
-    const breakfastBurgerImg = new Image();
-    breakfastBurgerImg.setAttribute('class', 'img');
-    breakfastBurgerImg.src = breakfastBurgerPic;
-
-    const breakfastBurgerRight = document.createElement('div');
-    breakfastBurgerRight.setAttribute('class', 'right');
-
-    const breakfastBurgerTitle = document.createElement('div');
-    breakfastBurgerTitle.setAttribute('class', 'title');
-    breakfastBurgerTitle.innerText = "Breakfast Burger - $13";
-
-    const breakfastBurgerText = document.createElement('div');
-    breakfastBurgerText.setAttribute('class', 'text');
-    breakfastBurgerText.innerText = "❖ Fried Egg\n ❖ White Cheddar Cheese\n ❖ Lettuce\n\
-                                     ❖ Bacon\n ❖ Tomato\n\ ❖ Caramelized Onion\n \
-                                     ❖ Toasted Brioche Bun\n ❖ Bacon Aoili";
-
-    breakfastBurgerRight.append(breakfastBurgerTitle);
-    breakfastBurgerRight.append(breakfastBurgerText);
-
-    breakfastBurger.append(breakfastBurgerImg);
-    breakfastBurger.append(breakfastBurgerRight);
-
-
-    // beef burger
-
-    const beefBurger = document.createElement('div');
-    beefBurger.setAttribute('id', 'beef-burger');
-    beefBurger.setAttribute('class', 'burger');
-
-    const beefBurgerImg = new Image();
-    beefBurgerImg.setAttribute('class', 'img');
-    beefBurgerImg.src = beefBurgerPic;
-
-    const beefBurgerRight = document.createElement('div');
-    beefBurgerRight.setAttribute('class', 'right');
-
-    const beefBurgerTitle = document.createElement('div');
-    beefBurgerTitle.setAttribute('class', 'title');
-    beefBurgerTitle.innerText = "Double Beef Burger - $18";
-
-    const beefBurgerText = document.createElement('div');
-    beefBurgerText.setAttribute('class', 'text');
-    beefBurgerText.innerText = "❖ Double Beef Patty\n ❖ White Cheddar Cheese\n ❖ Lettuce\n\
-                                ❖ Toasted Sesame Seed Bun\n ❖ Red Onion\n ❖ Garlic Aoili\n\
-                                ❖ Pickles\n ❖ Tomato\n";
-
-    beefBurgerRight.append(beefBurgerTitle);
-    beefBurgerRight.append(beefBurgerText);
-
-    beefBurger.append(beefBurgerImg);
-    beefBurger.append(beefBurgerRight);
-
-
-    // bison burger
-
-    const bisonBurger = document.createElement('div');
-    bisonBurger.setAttribute('id', 'bison-burger');
-    bisonBurger.setAttribute('class', 'burger');
-
-    const bisonBurgerImg = new Image();
-    bisonBurgerImg.setAttribute('class', 'img');
-    bisonBurgerImg.src = bisonBurgerPic;
-
-    const bisonBurgerRight = document.createElement('div');
-    bisonBurgerRight.setAttribute('class', 'right');
-
-    const bisonBurgerTitle = document.createElement('div');
-    bisonBurgerTitle.setAttribute('class', 'title');
-    bisonBurgerTitle.innerText = "Bison Burger - $17";
-
-    const bisonBurgerText = document.createElement('div');
-    bisonBurgerText.setAttribute('class', 'text');
-    bisonBurgerText.innerText = "❖ Grass Fed Bison Patty\n ❖ Swiss Cheese\n ❖ Lettuce\n ❖ Tomato\n\
-                                 ❖ Caramelized Onions\n ❖ Toasted Brioche Cun\n ❖ Jalepeno Aoili";
-
-    bisonBurgerRight.append(bisonBurgerTitle);
-    bisonBurgerRight.append(bisonBurgerText);
-
-    bisonBurger.append(bisonBurgerImg);
-    bisonBurger.append(bisonBurgerRight);
-
-
-    // chicken burger
-
-    const chickenBurger = document.createElement('div');
-    chickenBurger.setAttribute('id', 'chicken-burger');
-    chickenBurger.setAttribute('class', 'burger');
-
-    const chickenBurgerImg = new Image();
-    chickenBurgerImg.setAttribute('class', 'img');
-    chickenBurgerImg.src = chickenBurgerPic;
-
-    const chickenBurgerRight = document.createElement('div');
-    chickenBurgerRight.setAttribute('class', 'right');
-
-    const chickenBurgerTitle = document.createElement('div');
-    chickenBurgerTitle.setAttribute('class', 'title');
-    chickenBurgerTitle.innerText = "Chicken Burger - $15";
-
-    const chickenBurgerText = document.createElement('div');
-    chickenBurgerText.setAttribute('class', 'text');
-    chickenBurgerText.innerText = "❖ Double Fried Chicken\n ❖ Cheddar Cheese\n ❖ Lettuce\n\
-                                   ❖ Toasted Brioche Cun\n ❖ Sriracha Aoili";
-
-    chickenBurgerRight.append(chickenBurgerTitle);
-    chickenBurgerRight.append(chickenBurgerText);
-
-    chickenBurger.append(chickenBurgerImg);
-    chickenBurger.append(chickenBurgerRight);
-
-    // salmon burger
-
-    const salmonBurger = document.createElement('div');
-    salmonBurger.setAttribute('id', 'salmon-burger');
-    salmonBurger.setAttribute('class', 'burger');
-
-    const salmonBurgerImg = new Image();
-    salmonBurgerImg.setAttribute('class', 'img');
-    salmonBurgerImg.src = salmonBurgerPic;
-
-    const salmonBurgerRight = document.createElement('div');
-    salmonBurgerRight.setAttribute('class', 'right');
-
-    const salmonBurgerTitle = document.createElement('div');
-    salmonBurgerTitle.setAttribute('class', 'title');
-    salmonBurgerTitle.innerText = "Salmon Burger - $15";
-
-    const salmonBurgerText = document.createElement('div');
-    salmonBurgerText.setAttribute('class', 'text');
-    salmonBurgerText.innerText = "❖ Wild Salmon Patty\n ❖ Red Onion\n ❖ Lettuce\n ❖ Tomato\n\
-                                   ❖ Toasted Brioche Cun\n ❖ Remoulade Sauce";
-
-    salmonBurgerRight.append(salmonBurgerTitle);
-    salmonBurgerRight.append(salmonBurgerText);
-
-    salmonBurger.append(salmonBurgerImg);
-    salmonBurger.append(salmonBurgerRight);
-
-    menuBox.append(breakfastBurger);
-    menuBox.append(beefBurger);
-    menuBox.append(bisonBurger);
-    menuBox.append(chickenBurger);
-    menuBox.append(salmonBurger);
+    menuList.forEach((menuItem) => {
+        menuBox.append(addBurger(menuItem));
+    });
 
     const bottomHomeBorder = document.createElement('div');
     bottomHomeBorder.setAttribute('id', 'bottom-home-border');
@@ -190,3 +83,39 @@ function menu() {
 }
 
 export default menu;
+
+
+function addBurger(menuItem) {
+    const item = document.createElement('div');
+    item.setAttribute('id', `${menuItem.name}-burger`);
+    item.setAttribute('class', 'burger');
+
+    const itemImg = new Image();
+    itemImg.setAttribute('class', 'img');
+    itemImg.src = menuItem.picture;
+
+    const itemRight = document.createElement('div');
+    itemRight.setAttribute('class', 'right');
+
+    const itemTitle = document.createElement('div');
+    itemTitle.setAttribute('class', 'title');
+    itemTitle.innerText = `${menuItem.name} Burger - $${menuItem.price}`;
+
+    const itemText = document.createElement('div');
+    itemText.setAttribute('class', 'text');
+    
+    let text = "";
+    menuItem.ingredients.forEach((ingredient) => {
+        text += `❖ ${ingredient}\n`;
+    });
+
+    itemText.innerText = text;
+
+    itemRight.append(itemTitle);
+    itemRight.append(itemText);
+
+    item.append(itemImg);
+    item.append(itemRight);
+
+    return item;
+}
